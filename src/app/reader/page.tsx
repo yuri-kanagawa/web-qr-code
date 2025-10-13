7f341bd5ed6a0931040e0bc6d20810fc066e9a7b
kanagawa <77404155+yuri-kanagawa@users.noreply.github.com>
2025-10-14 03:35:03 +0900

    fix(reader): simplify import path for ReaderPage

--- src/app/reader/page.tsx
+++ src/app/reader/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Page as ReaderPage } from '@/ui/pages/reader/Page'
 import { Language } from '@/domains'
+import { Page as ReaderPage } from '@/ui/pages/reader'
 
 export default function Page() {
   return <ReaderPage language={Language.default()} />
