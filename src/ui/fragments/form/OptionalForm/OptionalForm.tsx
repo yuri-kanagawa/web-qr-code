aab50e881d5b0748f868b14a2ff12589d3be31b5
kanagawa <77404155+yuri-kanagawa@users.noreply.github.com>
2025-10-16 10:06:38 +0900

    OptionalFormでEyeSettings統合コンポーネントを使用
    - EyeColor1/2/3とEyeRadius1/2/3を削除
    - EyeSettings1/2/3に置き換えて関連設定を1ヶ所に集約
    - UIがシンプルで使いやすく、設定変更の効果を即座に確認可能

--- src/ui/fragments/form/OptionalForm/OptionalForm.tsx
+++ src/ui/fragments/form/OptionalForm/OptionalForm.tsx
@@ -1,15 +1,15 @@
 import { Language } from '@/domains/valueObjects/language'
 import { Stack } from '@/ui/cores'
-import { EcLevel } from '@/ui/fragments/form/OptionalForm/EcLevel'
-import { EyeColor1 } from '@/ui/fragments/form/OptionalForm/EyeColor1'
-import { EyeColor2 } from '@/ui/fragments/form/OptionalForm/EyeColor2'
-import { EyeColor3 } from '@/ui/fragments/form/OptionalForm/EyeColor3'
-import { FgColor } from '@/ui/fragments/form/OptionalForm/FgColor/FgColor'
-import { Logo } from '@/ui/fragments/form/OptionalForm/Logo'
-import { LogoPadding } from '@/ui/fragments/form/OptionalForm/LogoPadding'
-import { Opacity } from '@/ui/fragments/form/OptionalForm/Opacity'
 import { FC } from 'react'
 import { BgColor } from './BgColor'
+import { EcLevel } from './EcLevel'
+import { EyeSettings1 } from './EyeSettings1'
+import { EyeSettings2 } from './EyeSettings2'
+import { EyeSettings3 } from './EyeSettings3'
+import { FgColor } from './FgColor/FgColor'
+import { Logo } from './Logo'
+import { LogoPadding } from './LogoPadding'
+import { Opacity } from './Opacity'
 import { Size } from './Size'
 
 type Props = {
@@ -24,10 +24,9 @@ export const OptionalForm: FC<Props> = ({ file, setFile, language }) => {
       <BgColor language={language} />
       <FgColor language={language} />
       <EcLevel language={language} />
-      <EyeColor1 language={language} />
-      <EyeColor2 language={language} />
-      <EyeColor3 language={language} />
-      {/*<QRFinderPattern size={100} borderRadius={80} />*/}
+      <EyeSettings1 language={language} />
+      <EyeSettings2 language={language} />
+      <EyeSettings3 language={language} />
       <Logo file={file} setFile={setFile} language={language} />
       <Opacity file={file} language={language} />
       <LogoPadding language={language} file={file} />
