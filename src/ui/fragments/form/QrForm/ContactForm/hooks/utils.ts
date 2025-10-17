799bea857d819490901122eed0d06b569a173d4c
kanagawa <77404155+yuri-kanagawa@users.noreply.github.com>
2025-10-17 16:13:09 +0900

    refactor: vCard変換ロジックを改善してTYPE属性で個人用とビジネス用を区別

--- src/ui/fragments/form/QrForm/ContactForm/hooks/utils.ts
+++ src/ui/fragments/form/QrForm/ContactForm/hooks/utils.ts
@@ -1,18 +1,52 @@
 import { RegisterQrCodeContactSchema } from './zod'
 
 export const convertContact = (value: RegisterQrCodeContactSchema) => {
-  return `
-        BEGIN:VCARD
-        VERSION:3.0
-        FN:${value.lastName} ${value.middleName} ${value.firstName}
-        N:${value.lastName};${value.firstName}
-        ORG:${value.organization}
-        TITLE: ${value.post}
-        TEL;TYPE=CELL:+819012345678
-        TEL;TYPE=WORK:+81312345678
-        EMAIL:taro.yamada@example.com
-        ADR;TYPE=WORK:;;1-2-3 ABCビル;虎ノ門;港区;東京都;105-0001;日本
-        URL:${value.url}
-        END:VCARD
-        `
+  const fullName = [value.lastName, value.middleName, value.firstName]
+    .filter(Boolean)
+    .join(' ')
+
+  let vcard = `BEGIN:VCARD
+VERSION:3.0
+FN:${fullName}
+N:${value.lastName};${value.firstName};${value.middleName};;`
+
+  if (value.organization) {
+    vcard += `\nORG:${value.organization}`
+  }
+
+  if (value.post) {
+    vcard += `\nTITLE:${value.post}`
+  }
+
+  if (value.email) {
+    vcard += `\nEMAIL:${value.email}`
+  }
+
+  if (value.phoneNumber) {
+    vcard += `\nTEL;TYPE=HOME:${value.phoneNumber}`
+  }
+
+  if (value.workPhone) {
+    vcard += `\nTEL;TYPE=WORK:${value.workPhone}`
+  }
+
+  if (value.homeAddress) {
+    vcard += `\nADR;TYPE=HOME:;;${value.homeAddress};;;;`
+  }
+
+  if (value.workAddress) {
+    vcard += `\nADR;TYPE=WORK:;;${value.workAddress};;;;`
+  }
+
+  if (value.homeUrl) {
+    vcard += `\nURL;TYPE=HOME:${value.homeUrl}`
+  }
+
+  if (value.workUrl) {
+    vcard += `\nURL;TYPE=WORK:${value.workUrl}`
+  }
+
+  vcard += `\nEND:VCARD`
+
+  return vcard
 }
