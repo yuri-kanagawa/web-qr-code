cfc6069cd28f2ab8a0e31d60c1c82d7a252f2be1
kanagawa <77404155+yuri-kanagawa@users.noreply.github.com>
2025-10-22 23:21:31 +0900

    feat: QRFinderPatternコンポーネントをfeatures配下に追加

---
+++ src/features/qr-code/components/QRFinderPattern/RightTopQrFinderPattern/RightTopQrFinderPattern.tsx
@@ -0,0 +1,56 @@
+import { Language } from '@/domains/valueObjects/language'
+import { QrColor } from '@/domains/valueObjects/qrSettings'
+import { useWindowSize } from '@/hooks'
+import { ColorInput } from '@/ui/cores/input'
+import { CornerHighlightBox } from '@/ui/fragments/box'
+import { FC } from 'react'
+import { QRCode } from 'react-qrcode-logo'
+
+type Props = {
+  eyeColor2: string
+  setEyeColor2: (qrColor: QrColor) => void
+  label: string
+  language: Language
+}
+
+export const RightTopQrFinderPattern: FC<Props> = ({
+  eyeColor2,
+  setEyeColor2,
+  label,
+  language
+}) => {
+  const { isOverLaptop } = useWindowSize()
+  return (
+    <>
+      <ColorInput
+        format="hex"
+        value={eyeColor2}
+        label={label}
+        onChange={(value) => {
+          const result = QrColor.create(value, language)
+          if (result.isSuccess && result.qrColor) {
+            setEyeColor2(result.qrColor)
+          }
+        }}
+        isAlphaHidden={true}
+      />
+      {isOverLaptop && (
+        <CornerHighlightBox width={170} p={2}>
+          <QRCode
+            value={''}
+            size={150}
+            bgColor={'white'}
+            fgColor={'white'}
+            // ecLevel={ecLevel}
+            // logoImage={logoImage}
+            // logoOpacity={logoOpacity}
+            eyeRadius={[0, 0, 40]}
+            eyeColor={['white', eyeColor2, 'white']}
+            // logoPaddingStyle={logoPaddingStyle}
+            // logoPadding={9}
+          />
+        </CornerHighlightBox>
+      )}
+    </>
+  )
+}
