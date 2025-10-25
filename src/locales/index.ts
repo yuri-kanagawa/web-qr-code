1732566d0693d3be6fb4c8bc4410461a002331af
kanagawa <77404155+yuri-kanagawa@users.noreply.github.com>
2025-10-26 04:51:52 +0900

    fix: locales/index.tsの変更をコミット

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,7 +3,4 @@ export { en } from './en'
 export { fr } from './fr'
 export { ja } from './ja'
 
-// 設定
-export { Language } from '@/domains'
-export { languages } from './config/languages'
 export type { Locale } from './config/languages'
